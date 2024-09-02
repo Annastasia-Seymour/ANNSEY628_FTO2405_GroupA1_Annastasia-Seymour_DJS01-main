d7c35acd010415793fd58d84e812dd20bc643563
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-09-02 14:00:18 +0200

    commented out calcNewVel to see whats conflicting

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
+const d2 = d + (vel*time) //calculates new distance
 const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+//const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
+/*calcNewVel = (vel, acc, time) => { 
   return vel + (acc*time)
-}
+}*/
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
+//console.log(`Corrected New Velocity: ${vel2} km/h`);
+console.log("No banana");
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
 
