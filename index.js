146a66c6265a5f72ddabd47061c2f44398107291
Annastasia-Seymour <stasia1seymour@gmail.com>
2024-09-04 11:37:02 +0200

    Much googling yielded a formula

--- index.js
+++ index.js
@@ -14,22 +14,22 @@ const fuelRemaining = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const newDistance = distance + (velocityKmPerHour*timeInSeconds) //calculates new distance
-const newFuelRemaining = fuelBurnRate*timeInSeconds //calculates remaining fuel
-//const newVelocity = calcNewVel(acceleration, velocity, timeInSeconds) ; //calculates new velocity based on acceleration
+const newDistanceInKm = distanceInKm + velocityKmPerHour ; //calculates new distance
+const newFuelRemaining = fuelRemaining / fuelBurnRate;//calculates remaining fuel
+const newVelocity = calcNewVel(acceleration, velocityKmPerHour, timeInSeconds); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-/*calcNewVel = (acc, vel, time) => { 
-  return vel + (acc*time);
-}*/
 
-//console.log(`Corrected New Velocity: ${vel2} km/h`);//48880km/h
-console.log("No banana");
-console.log(`Corrected New Distance: ${velocityKmPerHour} km`);//10000Km
+function calcNewVel (acceleration, velocityKmPerHour, timeInSeconds){
+  return velocityKmPerHour + (acceleration*timeInSeconds) *3600/1000;
+}
+
+console.log(`Corrected New Velocity: ${newVelocity} km/h`);//48880km/h
+console.log(`Corrected New Distance: ${newDistanceInKm} km`);//10000Km
 console.log(`Corrected Remaining Fuel: ${newFuelRemaining} kg`);//3200kg
 
 
-
+calcNewVel();
 
 
 
